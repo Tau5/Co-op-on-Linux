d24ff58ab25700112fa791bfad0f88f5558c8aea
Pablo Alonso <tau0@tutanota.com>
2023-11-15 09:33:45 +0100

    Use sway instead of weston

--- Co-Op-On-Linux.sh
+++ Co-Op-On-Linux.sh
@@ -3,6 +3,7 @@
 DIR_CO_OP=$PWD
 DIR_CO_OP_WEST=$DIR_CO_OP/weston_configs
 DIR_CO_OP_CONT=$DIR_CO_OP/controller_blacklists
+DIR_CO_OP_SWAY=$DIR_CO_OP/sway_configs
 
 
 ### Currently only 2 players is supported 
@@ -53,13 +54,13 @@ CONTROLLER_2=$(zenity --list --title="Choose controller for player 2" --text="" 
 # add each device to blacklist
 for dev in $CONTROLLER_1
 do
-    printf -- '--blacklist=/dev/input/%s\n' $dev >> $DIR_CO_OP_CONT/Player1_Controller_Blacklist
+    printf -- '--blacklist=/dev/input/%s ' $dev >> $DIR_CO_OP_CONT/Player1_Controller_Blacklist
 done
 
 # add each device to blacklist
 for dev in $CONTROLLER_2
 do
-    printf -- '--blacklist=/dev/input/%s\n' $dev >> $DIR_CO_OP_CONT/Player2_Controller_Blacklist
+    printf -- '--blacklist=/dev/input/%s ' $dev >> $DIR_CO_OP_CONT/Player2_Controller_Blacklist
 done
 
 fi
@@ -93,7 +94,6 @@ cp $DIR_CO_OP_WEST/weston0.ini $DIR_CO_OP_WEST/weston1.ini
 cp $DIR_CO_OP_WEST/weston0.ini $DIR_CO_OP_WEST/weston2.ini
 fi
 
-
 ### Setup resolution for Weston sessions
 
 if [ "$1" = --quickrun ]; then
@@ -101,26 +101,50 @@ RESOLUTION=($2)
 else
 RESOLUTION=$(zenity --title="Resolution" --entry --text="Enter Resolution for Weston sessions ( for example: 1280x720 ) " --entry-text="1280x720")
 fi
+
 WIDTH=$(printf $RESOLUTION | awk -F "x" '{print $1}')
 HEIGHT=$(printf $RESOLUTION | awk -F "x" '{print $2}')
 
+exec_command1="WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player2_Controller_Blacklist ) $GAMERUN"
+exec_command2="WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player1_Controller_Blacklist ) $GAMERUN"
+
+mkdir $DIR_CO_OP_SWAY
+rm $DIR_CO_OP_SWAY/*.conf
+
+echo "default_border none 0" > $DIR_CO_OP_SWAY/sway0.conf
+echo "output WL-1 resolution $(($WIDTH*2))x$HEIGHT" >> $DIR_CO_OP_SWAY/sway0.conf
+echo "exec sway -c $DIR_CO_OP_SWAY/sway1.conf" >> $DIR_CO_OP_SWAY/sway0.conf
+echo "exec sway -c $DIR_CO_OP_SWAY/sway2.conf" >> $DIR_CO_OP_SWAY/sway0.conf
+
+echo "default_border none 0" > $DIR_CO_OP_SWAY/sway1.conf
+echo "output WL-1 resolution $(($WIDTH))x$HEIGHT" >> $DIR_CO_OP_SWAY/sway1.conf
+echo "exec $exec_command1" >> $DIR_CO_OP_SWAY/sway1.conf
+
+echo "default_border none 0" > $DIR_CO_OP_SWAY/sway2.conf
+echo "output WL-1 resolution $(($WIDTH))x$HEIGHT" >> $DIR_CO_OP_SWAY/sway2.conf
+echo "exec $exec_command2" >> $DIR_CO_OP_SWAY/sway2.conf
 
 ### Launching Weston sessions
-weston --xwayland -c "$DIR_CO_OP_WEST/weston1.ini" --width=$WIDTH --height=$HEIGHT 2> /dev/null &
-weston --xwayland -c "$DIR_CO_OP_WEST/weston2.ini" --width=$WIDTH --height=$HEIGHT 2> /dev/null &
+#weston --xwayland -c "$DIR_CO_OP_WEST/weston1.ini" --width=$WIDTH --height=$HEIGHT 2> /dev/null &
+#weston --xwayland -c "$DIR_CO_OP_WEST/weston2.ini" --width=$WIDTH --height=$HEIGHT 2> /dev/null &
+
+### Launching sway sessions
+
+sway -c $DIR_CO_OP_SWAY/sway0.conf &
+
+#WAYLAND_DISPLAY=wayland-1 sway -c $DIR_CO_OP_SWAY/sway1.conf &
+#WAYLAND_DISPLAY=wayland-1 sway -c $DIR_CO_OP_SWAY/sway2.conf &
+
 #notneeded=$(ps l | grep weston0.ini | awk 'length($0) > 120 {print $3}')
 #kill $notneeded
-sleep 1
-
-
-### Launching instances
+sleep 3
 
 #---Player 1---#
 #sleep 5
-DISPLAY=:2 WAYLAND_DISPLAY=wayland-1 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player2_Controller_Blacklist ) "$GAMERUN" &
+#DISPLAY=:3 WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player2_Controller_Blacklist ) "$GAMERUN" &
 #sleep 1
 #---Player 2---#
-DISPLAY=:3 WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player1_Controller_Blacklist ) "$GAMERUN" &
+#DISPLAY=:4 WAYLAND_DISPLAY=wayland-3 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player1_Controller_Blacklist ) "$GAMERUN" &
 #sleep 1
 
 
