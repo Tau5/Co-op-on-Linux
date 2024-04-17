8740f719f34ae295fa392191a5cc3f21cfda14ad
Tau <tau0@tutanota.com>
2024-04-17 19:54:28 +0200

    Update get-devices script

--- get-devices.py
+++ get-devices.py
@@ -1,4 +1,19 @@
+#!/usr/bin/env python
+
 import sys
+from pathlib import Path
+
+if len(sys.argv) < 2 or sys.argv[1] == "help":
+    print("""get-devices.py usage
+
+help: Display this help
+list-handlers: list all gamepad handlers
+list-names: list all gamepad names
+list-zenity: list all gamepad handlers and names on a format for zenity (Each line it displays name and the next, handler)
+list-zenity-exclude <handlers...>: like list-zenity but excludes the devices with the specified handlers
+list-handlers-exclude <handlers...>: list all gamepad handlers excluding the ones specified on the command arguments
+    """)
+    exit()
 
 f = open("/proc/bus/input/devices", "r")
 
@@ -11,8 +26,48 @@ for i in range(0, len(lines)):
     if (lines[i][0] == 'N'):
         if ("js" not in lines[i+4]): continue
         devices.append(lines[i][9:-2])
-        handlers.append(lines[i+4][12:-1])
+        devhandlers = []
 
-for i in range(0, len(devices)):
-    print(devices[i])
-    print(handlers[i])
+        syspath = lines[i+2][9:]
+        p = Path(syspath)
+        p = p.parent.parent / "hidraw"
+        if (p.exists()):
+            for i in p.iterdir():
+                devhandlers.append("/dev/"+i.name)
+
+        for h in lines[i+4][12:-1].split(" "):
+            devhandlers.append("/dev/input"+h)
+
+        handlers.append(devhandlers.join(" "))
+
+if sys.argv[1] == "list-handlers-exclude":
+    for a in range(2, len(sys.argv)):
+        for i in range(0, len(handlers)):
+            if (i > len(handlers)-1): break
+            if sys.argv[a] in handlers[i]:
+                del handlers[i]
+
+
+    for i in range(0, len(handlers)):
+        print(handlers[i])
+
+if sys.argv[1] == "list-handlers":
+    for i in range(0, len(handlers)):
+        print(handlers[i])
+
+if sys.argv[1] == "list-names":
+    for i in range(0, len(devices)):
+        print(devices[i])
+
+if sys.argv[1] == "list-zenity" or sys.argv[1] == "list-zenity-exclude":
+    if sys.argv[1] == "list-zenity-exclude":
+        for a in range(2, len(sys.argv)):
+            for i in range(0, len(handlers)):
+                if (i > len(handlers)-1): break
+                if sys.argv[a] in handlers[i]:
+                    del handlers[i]
+                    del devices[i]
+
+    for i in range(0, len(devices)):
+        print(devices[i])
+        print(handlers[i])
