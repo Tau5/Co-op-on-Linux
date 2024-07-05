038c62fb1cd6c45d2b935edb1111700f407671ed
Ice Yeti <ethanxbetz@gmail.com>
2024-07-05 04:08:13 -0400

    add option for 2 windows rather than 1 splitscreen window

--- Co-Op-On-Linux.sh
+++ Co-Op-On-Linux.sh
@@ -13,7 +13,6 @@ DIR_CO_OP=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 DIR_CO_OP_CONT=$DIR_CO_OP/controller_blacklists
 DIR_CO_OP_SWAY=$DIR_CO_OP/sway_configs
 
-
 ### Currently only 2 players is supported
 
 if [ -e steamos-check ]; then
@@ -26,26 +25,28 @@ if [ "$1" = --help ]; then
     echo "
     Co-Op-On-Linux.sh
 
-    Enviroment variables:
+    Environment variables:
 
-    WIDTH: width of total area in pixels
-    HEIGHT: height of total area in pixels
+    WIDTH: width of total area in pixels (only for single window)
+    HEIGHT: height of total area in pixels (only for single window)
+    WIDTH1: width of total area in pixels for player 1 (only for double window)
+    HEIGHT1: height of total area in pixels for player 1 (only for double window)
+    WIDTH2: width of total area in pixels for player 2 (only for double window)
+    HEIGHT2: height of total area in pixels for player 2 (only for double window)
     GAMERUN: command to run to start the game
     "
 fi
-
 
 DIALOG="zenity"
 
 ### Manage game controllers
 if [ "$DIR_CO_OP_CONT" != "/*" ]; then
-rm -rf $DIR_CO_OP_CONT
+    rm -rf $DIR_CO_OP_CONT
 else
-    echo "There was a bug were accidentally your system could have been wiped. Please report it"
+    echo "There was a bug where accidentally your system could have been wiped. Please report it"
 fi
 
 mkdir $DIR_CO_OP_CONT
-
 
 function select_controllers() {
     readarray -t CONTROLLERS < <( $DIR_CO_OP/get-devices.py list-zenity )
@@ -69,15 +70,12 @@ function select_controllers() {
     fi
 
     CONTROLLER_2=$(echo $zenity_out | uniq)
-    #CONTROLLER_2=$($DIALOG --list --title="Choose controller for player 2" --text="" --column=Controllers --column=devices \ "${CONTROLLERS[@]}" --print-column 2 | uniq)
 
-    # add each device to blacklist
     for dev in $CONTROLLER_1
     do
         printf -- '--blacklist=%s ' $dev >> $DIR_CO_OP_CONT/Player1_Controller_Blacklist
     done
 
-    # add each device to blacklist
     for dev in $CONTROLLER_2
     do
         printf -- '--blacklist=%s ' $dev >> $DIR_CO_OP_CONT/Player2_Controller_Blacklist
@@ -91,41 +89,62 @@ function select_controllers() {
     do
         printf -- '--blacklist=%s ' $dev >> $DIR_CO_OP_CONT/Global_Controller_Blacklist
     done
-};
-
-if [ -z $WIDTH ] || [ -z $HEIGHT ] || [ -z $GAMERUN ]; then
-    zenity --error --text "Enviroment variables not set (Did you run this without a preset?)"
-    exit
-fi
+}
 
 select_controllers
 
+if [ -n "$WIDTH" ] && [ -n "$HEIGHT" ]; then
+    exec_command1="WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player1_Controller_Blacklist ) $(cat $DIR_CO_OP_CONT/Global_Controller_Blacklist) $GAMERUN"
+    exec_command2="WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player2_Controller_Blacklist ) $(cat $DIR_CO_OP_CONT/Global_Controller_Blacklist) $GAMERUN"
 
-exec_command1="WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player2_Controller_Blacklist ) $(cat $DIR_CO_OP_CONT/Global_Controller_Blacklist) $GAMERUN"
-exec_command2="WAYLAND_DISPLAY=wayland-2 firejail --noprofile $(cat $DIR_CO_OP_CONT/Player1_Controller_Blacklist ) $(cat $DIR_CO_OP_CONT/Global_Controller_Blacklist) $GAMERUN"
+    mkdir $DIR_CO_OP_SWAY
+    rm $DIR_CO_OP_SWAY/*.conf
 
-mkdir $DIR_CO_OP_SWAY
-rm $DIR_CO_OP_SWAY/*.conf
+    echo "default_border none 0" > $DIR_CO_OP_SWAY/sway0.conf
+    echo "output WL-1 resolution ${WIDTH}x${HEIGHT}" >> $DIR_CO_OP_SWAY/sway0.conf
+    echo "output X11-1 resolution ${WIDTH}x${HEIGHT}" >> $DIR_CO_OP_SWAY/sway0.conf
+    echo "exec sway -c $DIR_CO_OP_SWAY/sway1.conf" >> $DIR_CO_OP_SWAY/sway0.conf
+    echo "exec sway -c $DIR_CO_OP_SWAY/sway2.conf" >> $DIR_CO_OP_SWAY/sway0.conf
 
-echo "default_border none 0" > $DIR_CO_OP_SWAY/sway0.conf
-echo "output WL-1 resolution $(($WIDTH))x$HEIGHT" >> $DIR_CO_OP_SWAY/sway0.conf
-# Set resolution for X11 and Xwayland compositors (eg. gamescope)
-echo "output X11-1 resolution $(($WIDTH))x$HEIGHT" >> $DIR_CO_OP_SWAY/sway0.conf
-echo "exec sway -c $DIR_CO_OP_SWAY/sway1.conf" >> $DIR_CO_OP_SWAY/sway0.conf
-echo "exec sway -c $DIR_CO_OP_SWAY/sway2.conf" >> $DIR_CO_OP_SWAY/sway0.conf
+    echo "default_border none 0" > $DIR_CO_OP_SWAY/sway1.conf
+    echo "output WL-1 resolution $((${WIDTH}/2))x${HEIGHT}" >> $DIR_CO_OP_SWAY/sway1.conf
+    echo "exec $exec_command1" >> $DIR_CO_OP_SWAY/sway1.conf
 
-echo "default_border none 0" > $DIR_CO_OP_SWAY/sway1.conf
-echo "output WL-1 resolution $(($WIDTH/2))x$HEIGHT" >> $DIR_CO_OP_SWAY/sway1.conf
-echo "exec $exec_command1" >> $DIR_CO_OP_SWAY/sway1.conf
+    echo "default_border none 0" > $DIR_CO_OP_SWAY/sway2.conf
+    echo "output WL-1 resolution $((${WIDTH}/2))x${HEIGHT}" >> $DIR_CO_OP_SWAY/sway2.conf
+    echo "exec $exec_command2" >> $DIR_CO_OP_SWAY/sway2.conf
 
-echo "default_border none 0" > $DIR_CO_OP_SWAY/sway2.conf
-echo "output WL-1 resolution $(($WIDTH/2))x$HEIGHT" >> $DIR_CO_OP_SWAY/sway2.conf
-echo "exec $exec_command2" >> $DIR_CO_OP_SWAY/sway2.conf
+    ### Launching sway sessions
+    cd $(dirname $GAMERUN)
+    echo $PWD
+    sway -c $DIR_CO_OP_SWAY/sway0.conf &
+else
+    if [ -z $WIDTH1 ] || [ -z $HEIGHT1 ] || [ -z $WIDTH2 ] || [ -z $HEIGHT2 ] || [ -z $GAMERUN ]; then
+        zenity --error --text "Environment variables not set (Did you run this without a preset?)"
+        exit
+    fi
 
-### Launching sway sessions
+    exec_command1="firejail --noprofile $(cat $DIR_CO_OP_CONT/Player1_Controller_Blacklist ) $(cat $DIR_CO_OP_CONT/Global_Controller_Blacklist) $GAMERUN"
+    exec_command2="firejail --noprofile $(cat $DIR_CO_OP_CONT/Player2_Controller_Blacklist ) $(cat $DIR_CO_OP_CONT/Global_Controller_Blacklist) $GAMERUN"
 
-cd $(dirname $GAMERUN)
-echo $PWD
-sway -c $DIR_CO_OP_SWAY/sway0.conf &
+    mkdir $DIR_CO_OP_SWAY
+    rm $DIR_CO_OP_SWAY/*.conf
+
+    echo "default_border none 0" > $DIR_CO_OP_SWAY/sway1.conf
+    echo "output WL-1 resolution ${WIDTH1}x${HEIGHT1}" >> $DIR_CO_OP_SWAY/sway1.conf
+    echo "output X11-1 resolution ${WIDTH1}x${HEIGHT1}" >> $DIR_CO_OP_SWAY/sway1.conf
+    echo "exec $exec_command1" >> $DIR_CO_OP_SWAY/sway1.conf
+
+    echo "default_border none 0" > $DIR_CO_OP_SWAY/sway2.conf
+    echo "output WL-1 resolution ${WIDTH2}x${HEIGHT2}" >> $DIR_CO_OP_SWAY/sway2.conf
+    echo "output X11-1 resolution ${WIDTH2}x${HEIGHT2}" >> $DIR_CO_OP_SWAY/sway2.conf
+    echo "exec $exec_command2" >> $DIR_CO_OP_SWAY/sway2.conf
+
+    ### Launching sway sessions
+    cd $(dirname $GAMERUN)
+    echo $PWD
+    sway -c $DIR_CO_OP_SWAY/sway1.conf &
+    sway -c $DIR_CO_OP_SWAY/sway2.conf &
+fi
 
 echo "Done~!"
--- create-new-preset.sh
+++ create-new-preset.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-#
+
 DIALOG=zenity
 DIR_CO_OP=$PWD
 
@@ -9,18 +9,41 @@ else
     GAMERUN=$(zenity --title="Select game executable/launch script" --file-selection)
 fi
 
-DEFAULT_RES=$(xdpyinfo | awk '/dimensions/{print $2}')
-RESOLUTION=$($DIALOG --title="Resolution" --entry --text="Enter Resolution for Weston sessions ( for example: 1280x720 ) " --entry-text=$DEFAULT_RES)
+NUM_WINDOWS=$($DIALOG --list --title="Number of Windows" --text="Select number of windows" --radiolist --column="Choose" --column="Option" TRUE "1 window" FALSE "2 windows")
 
-WIDTH=$(printf $RESOLUTION | awk -F "x" '{print $1}')
-HEIGHT=$(printf $RESOLUTION | awk -F "x" '{print $2}')
+if [ "$NUM_WINDOWS" = "1 window" ]; then
+    DEFAULT_RES=$(xdpyinfo | awk '/dimensions/{print $2}')
+    RESOLUTION=$($DIALOG --title="Resolution" --entry --text="Enter Resolution for Weston session (for example: 1280x720)" --entry-text=$DEFAULT_RES)
 
-name=$($DIALOG --title="Profile name" --entry --text="Enter a name for the profile" --entry-text="name")
-mkdir -p $DIR_CO_OP/profiles
-echo "export WIDTH=$WIDTH" > "$DIR_CO_OP/profiles/$name.sh"
-echo "export HEIGHT=$HEIGHT" >> "$DIR_CO_OP/profiles/$name.sh"
-echo "export GAMERUN='$GAMERUN'" >> "$DIR_CO_OP/profiles/$name.sh"
-echo "../Co-Op-On-Linux.sh" >> "$DIR_CO_OP/profiles/$name.sh"
-chmod +x "$DIR_CO_OP/profiles/$name.sh"
+    WIDTH=$(printf $RESOLUTION | awk -F "x" '{print $1}')
+    HEIGHT=$(printf $RESOLUTION | awk -F "x" '{print $2}')
 
-zenity --info --text "Preset created! To load it go to the preset folder and execute it's script"
+    name=$($DIALOG --title="Profile name" --entry --text="Enter a name for the profile" --entry-text="name")
+    mkdir -p $DIR_CO_OP/profiles
+    echo "export WIDTH=$WIDTH" > "$DIR_CO_OP/profiles/$name.sh"
+    echo "export HEIGHT=$HEIGHT" >> "$DIR_CO_OP/profiles/$name.sh"
+    echo "export GAMERUN='$GAMERUN'" >> "$DIR_CO_OP/profiles/$name.sh"
+    echo "exec ../Co-Op-On-Linux.sh" >> "$DIR_CO_OP/profiles/$name.sh"
+    chmod +x "$DIR_CO_OP/profiles/$name.sh"
+else
+    DEFAULT_RES=$(xdpyinfo | awk '/dimensions/{print $2}')
+    RESOLUTION1=$($DIALOG --title="Resolution for Player 1" --entry --text="Enter Resolution for Weston session for Player 1 (e.g., 1280x720)" --entry-text=$DEFAULT_RES)
+    RESOLUTION2=$($DIALOG --title="Resolution for Player 2" --entry --text="Enter Resolution for Weston session for Player 2 (e.g., 1280x720)" --entry-text=$DEFAULT_RES)
+
+    WIDTH1=$(printf $RESOLUTION1 | awk -F "x" '{print $1}')
+    HEIGHT1=$(printf $RESOLUTION1 | awk -F "x" '{print $2}')
+    WIDTH2=$(printf $RESOLUTION2 | awk -F "x" '{print $1}')
+    HEIGHT2=$(printf $RESOLUTION2 | awk -F "x" '{print $2}')
+
+    name=$($DIALOG --title="Profile name" --entry --text="Enter a name for the profile" --entry-text="name")
+    mkdir -p $DIR_CO_OP/profiles
+    echo "export WIDTH1=$WIDTH1" > "$DIR_CO_OP/profiles/$name.sh"
+    echo "export HEIGHT1=$HEIGHT1" >> "$DIR_CO_OP/profiles/$name.sh"
+    echo "export WIDTH2=$WIDTH2" >> "$DIR_CO_OP/profiles/$name.sh"
+    echo "export HEIGHT2=$HEIGHT2" >> "$DIR_CO_OP/profiles/$name.sh"
+    echo "export GAMERUN='$GAMERUN'" >> "$DIR_CO_OP/profiles/$name.sh"
+    echo "exec ../Co-Op-On-Linux.sh" >> "$DIR_CO_OP/profiles/$name.sh"
+    chmod +x "$DIR_CO_OP/profiles/$name.sh"
+fi
+
+zenity --info --text "Preset created! To load it go to the preset folder and execute its script"
